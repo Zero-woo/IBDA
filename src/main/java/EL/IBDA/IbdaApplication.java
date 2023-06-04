1f8892725d4ffc11c28d92f80b7814cb59e8aff8
youngwoo <owq0522@gmail.com>
2023-06-04 13:11:16 +0900

    기본환경 세팅

---
+++ src/main/java/EL/IBDA/IbdaApplication.java
@@ -0,0 +1,13 @@
+package EL.IBDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EL/IBDA/IbdaApplicationTests.java
@@ -0,0 +1,13 @@
+package EL.IBDA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
